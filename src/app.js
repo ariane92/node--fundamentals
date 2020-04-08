638c4d85883fac6b96e3447396daa09c6785a939
Ariane Mateus <ariane.ccomp@gmail.com>
2020-04-08 11:58:53 -0300

    routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,72 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
+  
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs} = request.body;
+
+  const repositorie = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    like: 0,
+  }
+
+  if(!isUuid(repositorie.id)){
+    return res.status(400).json({ error: 'Invalid project id.'});
+  }
+
+  repositories.push(repositorie);
+  return response.json(repositorie);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs} = request.body;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id == id);
+
+  if(repositorieIndex < 0){
+    return response.json({error: 'Project Not Found.'})
+  }
+
+  const repositorie = {
+    title, 
+    url, 
+    techs
+  }
+
+  repositories[repositorieIndex] = repositorie;
+  return response.json(repositorie);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+ 
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id == id);
+
+  if(repositorieIndex < 0){
+    return response.json({error: 'Project Not Found.'});
+  }
+
+  repositories.splice(repositorieIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+ 
+  const repositorie = repositories.find(repositorie => repositorie.id == id);
+
+  repositorie.like++;
+  return response.json(repositorie)
+
 });
 
 module.exports = app;
